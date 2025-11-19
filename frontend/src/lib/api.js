83c0b2ad0884f4900150ce54efd8490f0237356d
Suren7702 <suren7702@gmail.com>
2025-11-19 22:29:07 +0530

    The Api file is changed

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,3 +1,22 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL, headers: { 'Content-Type': 'application/json' } });
+
+const API = axios.create({
+  // This reads the variable you set in Vercel
+  baseURL: process.env.NEXT_PUBLIC_API_URL, 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Automatically add the token to every request if we are logged in
+API.interceptors.request.use((req) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      req.headers['x-auth-token'] = token;
+    }
+  }
+  return req;
+});
+
 export default API;
